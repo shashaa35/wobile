30965ff63ed0d903ce8e5aa48e9e6566bac6f71c
aayusharora <aayush113002@gmail.com>
2016-04-09 04:00:28 +0530

    new files

--- js/app.js
+++ js/app.js
@@ -12,10 +12,33 @@ controller:'mycontroller'
 }]);
 app.controller('mycontroller', function ($scope, $http) {
 	$scope.showing=true;
+	 $scope.name="";
+    $scope.password="";
+
+	
+
 	$scope.show=function(){
 	$scope.showing=!$scope.showing;
-	var signup=function(){
-		console.log("working")			
+	
+	}
+    $scope.signup=function(name,password,email){
+		console.log("working");
+        $scope.name=name,
+        $scope.password=password,
+		$scope.details={
+		name:$scope.name,
+        password:$scope.password,
+        email:$scope.email
+	    }	
+	    var res=$http.post('index.php/signup',$scope.details);
+	    	res.success(function(data, status, headers, config) {
+           
+			$scope.message = data;
+		});
+		res.error(function(data, status, headers, config) {
+			alert( "failure message: " + JSON.stringify({data: data}));
+		});	
+
 		$http.get('signup').success (function(data){
              console.log(data);
 	         }).
@@ -23,6 +46,9 @@ app.controller('mycontroller', function ($scope, $http) {
 		console.log("error");
 	})
 	}
+	 $scope.login=function(){	
+     	var res=$http.post('index.php/signup',$scope.details);
+     
 	}
 
 });
